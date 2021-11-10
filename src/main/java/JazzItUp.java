7f87f412be36405e77ec69bd297bb843190b8b90
martin@phew.se <martin@phew.se>
2021-11-10 01:03:41 +0100

    Add solution to Jazz It Up (with link to problem)

--- src/main/java/JazzItUp.java
+++ src/main/java/JazzItUp.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// https://open.kattis.com/problems/jazzitup
+
 public class JazzItUp {
 
     public static void main(String[] args) throws IOException {
