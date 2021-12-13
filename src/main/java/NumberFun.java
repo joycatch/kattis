9fb72f09957887c2a7bd00834fb9ea5fe4db9331
martin@phew.se <martin@phew.se>
2021-12-13 21:14:17 +0100

    Add solution to Number Fun

--- src/main/java/NumberFun.java
+++ src/main/java/NumberFun.java
@@ -7,14 +7,11 @@ import java.io.InputStreamReader;
 public class NumberFun {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < Integer.parseInt(br.readLine()); i++) {
             String[] split = br.readLine().split(" ");
             int a = Integer.parseInt(split[0]);
             int b = Integer.parseInt(split[1]);
             int c = Integer.parseInt(split[2]);
-
             boolean possible = (Math.abs(a - b) == c)
                     || (a + b == c)
                     || (a * b == c)
